062f5435f84a53a73344dd4bbcc562f76e349794
Zeldruck <felix.jouannneau@gmail.com>
2023-07-22 14:42:27 +0200

    Scene field

---
+++ Runtime/Editor/SceneFieldEditor.cs
@@ -0,0 +1,74 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Zeldruck.Packages
+{
+    [CustomPropertyDrawer(typeof(SceneField))]
+    public class SceneFieldEditor : PropertyDrawer
+    {
+
+        private SceneAsset _scene;
+        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty scenePath = property.FindPropertyRelative("scenePath");
+            SerializedProperty sceneBuildId = property.FindPropertyRelative("sceneBuildIndex");
+            
+            _scene = AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath.stringValue);
+
+            EditorGUI.BeginChangeCheck();
+
+            string labelName = property.name;
+            labelName = labelName[0].ToString().ToUpper() + labelName.Substring(1);
+            
+            var newScene = EditorGUI.ObjectField(position, labelName, _scene, typeof(SceneAsset), false) as SceneAsset;
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                if (newScene == null)
+                    return;
+                
+                var newPath = AssetDatabase.GetAssetPath(newScene);
+                scenePath.stringValue = newPath;
+
+                TryAddSceneToBuildIndex(newPath, out bool success, out bool error, out string message);
+
+                if (!error)
+                    sceneBuildId.intValue = SceneUtility.GetBuildIndexByScenePath(newPath);
+                
+                property.serializedObject.ApplyModifiedProperties();
+            }
+        }
+        
+        private void TryAddSceneToBuildIndex(string scenePath, out bool success, out bool error, out string message)
+        {
+            int newBuildIndex = SceneUtility.GetBuildIndexByScenePath(scenePath);
+
+            if (newBuildIndex >= 0)
+            {
+                success = false;
+                error = false;
+                message = "Scene already referenced in the build index";
+                
+                return;
+            }
+            
+            
+            var currentBuildScenes = EditorBuildSettings.scenes;
+            var newSettingsScenes = new EditorBuildSettingsScene[currentBuildScenes.Length + 1];
+                    
+            System.Array.Copy(currentBuildScenes, newSettingsScenes, currentBuildScenes.Length);
+
+            var newSettingScene = new EditorBuildSettingsScene(scenePath, true);
+
+            newSettingsScenes[^1] = newSettingScene;
+
+            EditorBuildSettings.scenes = newSettingsScenes;
+
+            success = true;
+            error = false;
+            message = "Scene has been added to build index";
+        }
+    }
+}
---
+++ Runtime/SceneField.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Zeldruck.Packages
+{
+    [Serializable]
+    public class SceneField
+    {
+        public string scenePath;
+        public int sceneBuildIndex;
+    }
+}
